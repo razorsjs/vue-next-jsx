de5cb7782399b71d10a7f13a0e32564c04445a88
liujia02 <bit_liujia@163.com>
2019-06-10 18:20:47 +0800

    delete unusual plugin

--- packages/babel-plugin-safe-object/test/fixtures/set-and-get/actual.js
+++ packages/babel-plugin-safe-object/test/fixtures/set-and-get/actual.js
@@ -1,3 +1,4 @@
 const obj = {};
 obj.a.b = 1
 const y = obj.x.y
+a.b.c = z.x.c
--- packages/babel-plugin-safe-object/test/fixtures/set-and-get/expected.js
+++ packages/babel-plugin-safe-object/test/fixtures/set-and-get/expected.js
@@ -5,3 +5,4 @@ var _babelPluginSafeObject = require("@razors/babel-plugin-safe-object");
 var obj = {};
 (0, _babelPluginSafeObject.safeSet)(obj, ["a", "b"], 1);
 var y = (0, _babelPluginSafeObject.safeGet)(obj, ["x", "y"]);
+(0, _babelPluginSafeObject.safeSet)(a, ["b", "c"], (0, _babelPluginSafeObject.safeGet)(z, ["x", "c"]));

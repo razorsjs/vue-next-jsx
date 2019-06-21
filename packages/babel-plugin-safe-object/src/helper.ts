f1959de86a5d9836530e5d9cefef4eb246594ce6
liujia02 <bit_liujia@163.com>
2019-06-21 14:51:51 +0800

    rename undefined

--- packages/babel-plugin-safe-object/src/helper.ts
+++ packages/babel-plugin-safe-object/src/helper.ts
@@ -1,6 +1,7 @@
 export function safeGet(obj: object | Array<any>, properties?: Array<string | number>) {
   // will return undefined when obj is not object or array
   // will return 0 if last property is length
+  const undefined = void 0;
   if (!properties || properties.length === 0) return undefined;
   return properties.reduce((pre: any, value, index: number) => {
     return pre !== undefined ? pre[value] : ((value === 'length' && index === properties.length - 1) ? 0 : undefined);
@@ -8,6 +9,7 @@ export function safeGet(obj: object | Array<any>, properties?: Array<string | nu
 }
 
 export function safeSet(obj: object | Array<any>, properties: Array<string | number>, val?: any) {
+  const undefined = void 0;
   if (properties.length === 0 || val === undefined) return;
   return properties.reduce((pre: any, value, index: number) => {
     if (pre[value] === undefined) {
